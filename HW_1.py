d118dc529e28c6d8ad331bb13442930c83e2161c
Regakirush <regakirush@yandex.ru>
2022-01-12 22:11:24 +0700

    add two files HW by Python

---
+++ HW_1.py
@@ -0,0 +1,37 @@
+a = 'Regina'
+print(type(a), a)
+
+age = 23
+print(type(age), age)
+
+c = 1.57
+print(type(c), c)
+
+b = 8
+print(type(b), b)
+
+shop = ['milk', 'fish', 'cucumbers', 'bananas']
+print(type(shop), shop)
+
+information = ('name', 'surname', 'age')
+print(type(information), information)
+
+country = set(['Russia', 'Germany', 'China'])
+print(type(country), country)
+
+pets = frozenset(['d', 'g', 'd'])
+print(type(pets), pets)
+
+ab = {'Jess': 19, 'Peter': 22, 'Andrew': 25}
+print(type(ab), ab)
+
+str1 = 2
+str2 = 3
+str3 = str1 + str2
+print (str3)
+
+print (a, age)
+
+
+
+

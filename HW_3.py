dc7088fefa8bc7520a4cb11a2f599fab67f4eace
Regakirush <regakirush@yandex.ru>
2022-01-18 19:32:11 +0700

    add new homework №3

---
+++ HW_3.py
@@ -0,0 +1,57 @@
+# Создать переменную int_item со значением 10
+int_item = 10
+
+# Создать переменную comp_item со значением 1
+comp_item = 1
+
+# Создать переменную mult_int в которй умножите int_item на 2
+mult_int = int_item * 2
+
+# Создать переменную item_2 со значением True
+item_2 = False
+
+# Создать переменную item_3 со значением False
+item_3 = False
+
+# Создать переменную item_4 со значением 0
+item_4 = 0
+
+# Создать переменную item_5 со значением 1
+item_5 = 1
+
+# Создать переменную usd_item со значением ‘usd’
+usd_item = "usd"
+
+# Создать переменную usd_usd_rate со значением 1
+usd_usd_rate = 1
+
+# Создать переменную eur_item со значением ‘eur’
+eur_item = "eur"
+
+# Создать переменную usd_eur_rate со значением 0.86
+usd_eur_rate = 0.86
+
+# Создать переменную uah_item со значением ‘uah’
+uah_item = "uah"
+
+# Создать переменную usd_uah_rate со значением 26.23
+usd_ush_rate = 26.23
+
+# Создать переменную chf_item со значением ‘chf’
+chf_item = 'chf'
+
+# Создать переменную usd_chf_rate со значением 0.91
+usd_chf_rate = 0.91
+
+# Создать переменную rub_item со значением ‘rub’
+rub_item = 'rub'
+
+# Создать переменную usd_rub_rate со значением 71.88
+usd_rub_rate = 71.88
+
+# Создать переменную byn_item со значением ‘byn’
+byn_item = 'byn'
+
+# Создать переменную usd_byn_rate со значением 2.46
+usd_byn_rate = 2.46
+
